7f17f95356d87ed7edc0b3762fe94ac85e510210
gabrielnhn <ielnascarella@hotmail.com>
2022-04-11 11:35:12 -0300

    servers work

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server cache
+# PORT = 65432  # The port used by the server cache
+PORT = 65000  # The port used by the server cache
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 import random
+import socket
+import os
+
+HOST = "127.0.0.1"
+PORTS = [65000, 65001, 65002]
 
 def get_temperature(weather):
     if weather == "hot":
@@ -7,6 +12,18 @@ def get_temperature(weather):
         return random.randint(-5, 10)
 
 if __name__ == "__main__":
+    retval = os.fork() # Get First + Second
+
+    if retval != 0:
+        retval = os.fork() # Get Third
+
+        if retval != 0: # First
+            PORT = PORTS[0]
+        else: # Third
+            PORT = PORTS[2]
+
+    else: # Second
+        PORT = PORTS[1]
 
     if random.randint(0, 1):
         weather = "hot"
@@ -14,4 +31,18 @@ if __name__ == "__main__":
         weather = "cold"
 
 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Internet, TCP
 
+        s.bind((HOST, PORT))
+        
+        # Wait for client
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(str(get_temperature(weather)).encode("utf-8"))
